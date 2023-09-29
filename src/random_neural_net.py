2496850a30744f2b3755f92f23eec329020c716e
Charlotte Aten <charlotte.aten@du.edu>
2023-09-29 10:35:47 -0600

    Reorganization and documentation

--- src/random_neural_net.py
+++ src/random_neural_net.py
@@ -2,9 +2,32 @@
 Tools for creating random neural nets
 """
 
-import random as rand
+import random
 from discrete_neural_net import Neuron, Layer, NeuralNet
-from operations import RandomOperation, Identity
+from operations import Operation, Identity
+
+
+class RandomOperation(Operation):
+    """
+    A random operation. The values of the operation on its arguments are chosen randomly and lazily, but they are
+    memoized so that the operation is well-defined.
+    """
+
+    def __init__(self, order, arity):
+        """
+        Create a random operation of a given arity and order.
+
+        Arguments:
+            order (int): The size of the universe.
+            arity (int): The arity of the operation.
+        """
+
+        if arity == 0:
+            # For a nullary operation, we choose a random member of the universe to be the corresponding constant.
+            random_constant = random.randint(0, order - 1)
+            Operation.__init__(self, 0, random_constant)
+        else:
+            Operation.__init__(self, arity, lambda *x: random.randint(0, order - 1))
 
 
 class RandomNeuron(Neuron):
@@ -17,8 +40,8 @@ class RandomNeuron(Neuron):
 
         """
 
-        activation_func = rand.choice(basic_ops[rand.choice(tuple(basic_ops.keys()))])
-        Neuron.__init__(self, activation_func, [rand.choice(previous_layer.neurons)
+        activation_func = random.choice(basic_ops[random.choice(tuple(basic_ops.keys()))])
+        Neuron.__init__(self, activation_func, [random.choice(previous_layer.neurons)
                                                 for _ in range(activation_func.arity)])
 
 
@@ -51,11 +74,11 @@ class RandomNeuralNet(NeuralNet):
         basic_ops = {n: [RandomOperation(order, n) for _ in range(signature[n])] for n in signature.keys()}
         nonidentity_basic_ops = basic_ops.copy()
         if 0 in basic_ops.keys():
-            basic_ops[1].append(Identity(order))
+            basic_ops[1].append(Identity())
         else:
-            basic_ops[1] = [Identity(order)]
+            basic_ops[1] = [Identity()]
         architecture = [Layer(inputs)]
         for _ in range(depth - 2):
-            architecture.append(RandomLayer(basic_ops, architecture[-1], rand.randint(1, breadth)))
+            architecture.append(RandomLayer(basic_ops, architecture[-1], random.randint(1, breadth)))
         architecture.append(RandomLayer(nonidentity_basic_ops, architecture[-1], outputs))
         NeuralNet.__init__(self, architecture)
